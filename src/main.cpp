9abf442e0921268f402d41919d69aaa8b7fe21d5
Juni May <juni_may@outlook.com>
2022-05-20 16:11:39 +0800

    feat(character): move

--- src/main.cpp
+++ src/main.cpp
@@ -1,102 +1,44 @@
-/*******************************************************************************************
- *
- *   raylib [core] example - 3d camera first person
- *
- *   This example has been created using raylib 1.3 (www.raylib.com)
- *   raylib is licensed under an unmodified zlib/libpng license (View raylib.h
- *for details)
- *
- *   Copyright (c) 2015 Ramon Santamaria (@raysan5)
- *
- ********************************************************************************************/
-
 #include "raylib.h"
 
-#define MAX_COLUMNS 20
-
 int main(void) {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
 
-    InitWindow(screenWidth, screenHeight,
-               "raylib [core] example - 3d camera first person");
+    InitWindow(screenWidth, screenHeight, "HackPKU");
 
-    // Define the camera to look into our 3d world (position, target, up vector)
-    Camera camera = {0};
-    camera.position = {4.0f, 2.0f, 4.0f};
-    camera.target = {0.0f, 1.8f, 0.0f};
-    camera.up = {0.0f, 1.0f, 0.0f};
-    camera.fovy = 60.0f;
-    camera.projection = CAMERA_PERSPECTIVE;
+    Texture2D tiger = LoadTexture("assets/tiger.png");
+    Vector2 tiger_pos = { 800, 500 };
 
-    // Generates some random columns
-    float heights[MAX_COLUMNS] = {0};
-    Vector3 positions[MAX_COLUMNS] = {0};
-    Color colors[MAX_COLUMNS] = {0};
 
-    for (int i = 0; i < MAX_COLUMNS; i++) {
-        heights[i] = (float)GetRandomValue(1, 12);
-        positions[i] = {(float)GetRandomValue(-15, 15), heights[i] / 2.0f,
-                        (float)GetRandomValue(-15, 15)};
-        colors[i] = {(unsigned char)GetRandomValue(20, 255),
-                            (unsigned char)GetRandomValue(10, 55), 30, 255};
-    }
+    SetTargetFPS(60);
 
-    SetCameraMode(camera,
-                  CAMERA_FIRST_PERSON);  // Set a first person camera mode
+    while (!WindowShouldClose()) {
 
-    SetTargetFPS(60);  // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose()) {  // Detect window close button or ESC key
-        // Update
-        //----------------------------------------------------------------------------------
-        UpdateCamera(&camera);  // Update camera
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        BeginMode3D(camera);
-
-        DrawPlane({0.0f, 0.0f, 0.0f}, {32.0f, 32.0f},
-                  LIGHTGRAY);  // Draw ground
-        DrawCube({-16.0f, 2.5f, 0.0f}, 1.0f, 5.0f, 32.0f,
-                 BLUE);  // Draw a blue wall
-        DrawCube({16.0f, 2.5f, 0.0f}, 1.0f, 5.0f, 32.0f,
-                 LIME);  // Draw a green wall
-        DrawCube({0.0f, 2.5f, 16.0f}, 32.0f, 5.0f, 1.0f,
-                 GOLD);  // Draw a yellow wall
-
-        // Draw some cubes around
-        for (int i = 0; i < MAX_COLUMNS; i++) {
-            DrawCube(positions[i], 2.0f, heights[i], 2.0f, colors[i]);
-            DrawCubeWires(positions[i], 2.0f, heights[i], 2.0f, MAROON);
+        if (IsKeyDown(KEY_W)) {
+            tiger_pos.y -= 5;
+        }
+        if (IsKeyDown(KEY_S)) {
+            tiger_pos.y += 5;
+        }
+        if (IsKeyDown(KEY_A)) {
+            tiger_pos.x -= 5;
+        }
+        if (IsKeyDown(KEY_D)) {
+            tiger_pos.x += 5;
         }
 
-        EndMode3D();
+        BeginDrawing();
 
-        DrawRectangle(10, 10, 220, 70, Fade(SKYBLUE, 0.5f));
-        DrawRectangleLines(10, 10, 220, 70, BLUE);
+        ClearBackground(WHITE);
 
-        DrawText("First person camera default controls:", 20, 20, 10, BLACK);
-        DrawText("- Move with keys: W, A, S, D", 40, 40, 10, DARKGRAY);
-        DrawText("- Mouse move to look around", 40, 60, 10, DARKGRAY);
+
+        DrawTextureEx(tiger, tiger_pos, 0, 5.0, WHITE);
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();  // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    UnloadTexture(tiger);
 
+    CloseWindow();
     return 0;
 }
